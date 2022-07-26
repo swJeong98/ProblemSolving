c777bd5ba7b441c60d4ba0e1a8e54b17bff24161
swJeong98 <86451540+swJeong98@users.noreply.github.com>
2022-07-26 22:51:55 +0900

    Update 2473.cpp

--- Lecture/0x13_BinarySearch/solutions/2473.cpp
+++ Lecture/0x13_BinarySearch/solutions/2473.cpp
@@ -1,24 +1,49 @@
-/*BOJ 10815 숫자 카드 - 2022.03.13*/
+/* BOJ 2473 세 용액 - 2022.05.13 */
 #include <bits/stdc++.h>
 using namespace std;
 
-int A[500005];
+long long A[5005];
+int ans[3];
 
 int main()
 {
     ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+    cin.tie(0); cout.tie(0); 
 
     int N; cin>>N;
     for(int i=0;i<N;i++) cin>>A[i];
-    sort(A,A+N);
+    sort(A, A+N);
+    
+    long long mn = 3e9 + 5;
+    for(int i=0;i<N;i++){
+        for(int j=0;j<i;j++){
+            int idx = lower_bound(A, A+N, -(A[i]+A[j])) - A;
 
-    int M; cin>>M;
-    for(int i=0;i<M;i++){
-        int target; cin>>target;
-        auto st = lower_bound(A,A+N,target);
-        auto en = upper_bound(A,A+N,target);
-        cout << ( (en-st>0)? 1 : 0 ) << ' ';
+            if(idx == N){
+                while(idx == N || idx == i || idx == j) idx--;
+                if(abs(mn) > abs(A[i]+A[j]+A[idx])){
+                    mn = abs(A[i]+A[j]+A[idx]);
+                    ans[0] = A[i], ans[1] = A[j], ans[2] = A[idx];
+                    continue;
+                }
+            }
+            if(idx != 0 && idx-1 != i && idx-1 != j && abs(mn) > abs(A[i]+A[j]+A[idx-1])){
+                mn = abs(A[i]+A[j]+A[idx-1]);
+                ans[0] = A[i], ans[1] = A[j], ans[2] = A[idx-1];
+            }
+
+            if(idx != i && idx != j && abs(mn) > abs(A[i]+A[j]+A[idx])){
+                mn = abs(A[i]+A[j]+A[idx]);
+                ans[0] = A[i], ans[1] = A[j], ans[2] = A[idx];
+            }
+
+            if(idx != N-1 && idx+1 != i && idx+1 != j && abs(mn) > abs(A[i]+A[j]+A[idx+1])){
+                mn = abs(A[i]+A[j]+A[idx+1]);
+                ans[0] = A[i], ans[1] = A[j], ans[2] = A[idx+1];
+            }
+        }
     }
-    return 0;
+    
+    sort(ans,ans+3);
+    for(int i=0;i<3;i++) cout<<ans[i]<<' ';
 }
